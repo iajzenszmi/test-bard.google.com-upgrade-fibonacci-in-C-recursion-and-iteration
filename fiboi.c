ecbefcbe1f7860f8f78ec3f9764485a6aacc4da0
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2024-02-03 06:26:03 +1100

    Add files via upload

---
+++ fiboi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+  printf("Enter the number of terms: ");
+  scanf("%d", &n);
+
+  int a = 0, b = 1, next;
+
+  for (int i = 0; i < n; i++) {
+    printf("%d ", a);
+    next = a + b;
+    a = b;
+    b = next;
+  }
+
+  printf("\n");
+  return 0;
+}
